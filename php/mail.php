03cfcf1c4fee241ebc3854a4930372df64b9ecf2
Bettina Pulliam <bfpulliam@gmail.com>
2019-09-18 19:40:40 -0500

    correction to email

--- php/mail.php
+++ php/mail.php
@@ -4,7 +4,7 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 $subject = $_POST['subject'];
 $content="From: $name \n Email: $email \n Message: $message";
-$recipient = "youremail@here.com";
+$recipient = "bfpulliam@gmail.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
 echo "Email sent!";
