6e5ba1fdc8efe663b580230c799aa7976adce910
Bettina Pulliam <bfpulliam@gmail.com>
2020-09-16 16:31:39 -0500

    satisfied with layout of project sections:

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,1 +1,14 @@
 
+$(document).ready(function(){
+        $('[data-spy="scroll"]').each(function () {
+            var $spy = $(this).scrollspy('refresh');
+        });
+});
+
+
+$(document).ready(function () {
+    $("#myNavbar").on("activate.bs.scrollspy", function () {
+        var currentItem = $(".nav li.active > a").text();
+        $("#info").empty().html("Currently you are viewing - " + currentItem);
+    })
+});
